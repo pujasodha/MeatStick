6f4e4a59d1a7a041a596c1c7fbc4691dd1a877a1
John De Jesus <johndejesus20@rocketmail.com>
2018-06-20 20:50:11 -0700

    added

---
+++ client/src/Component/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import './Footer.js';
+
+
+class Footer extends Component {
+
+
+	render(){
+		return (
+			<div className ="Footer">
+	
+		)}
+	}
+
+export default Footer
