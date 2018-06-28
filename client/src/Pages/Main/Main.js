2efe228f1297e215a0c2ffec6603ac2339b53987
John De Jesus <jdeje002@gmail.com>
2018-06-27 18:35:20 -0700

    Added routes pages com

---
+++ client/src/Pages/Main/Main.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Fullimage from "../../Component/Fullimage";
+import ProbePic from "../../Images/DSC_9455.png";
+import WifiBridge from "../../Images/Bridgecopy.png";
+import Fullimage2 from "../../Component/Fullimage2";
+import HalfImageRow from "../../Component/HalfImageRow";
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="App">
+     
+      <Fullimage>{ProbePic}</Fullimage>
+      <Fullimage2>{WifiBridge}</Fullimage2>
+      <HalfImageRow></HalfImageRow> 
+      
+      </div>
+    );
+  }
+}
+
+export default Main;
