5e8ca64a4464d2cb87566a88c7a59fef1c4591c8
Christina Wu <CHRISTINASARAHWU@GMAIL.COM>
2018-06-30 10:59:58 -0700

    meatstick page added

---
+++ client/src/Pages/Meatstick/Meatstick.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import SubNav from '../../Component/SubNav';
+
+
+class Meatstick extends Component {
+  render() {
+    return (
+      <div className="App">
+      <SubNav></SubNav>
+      
+      </div>
+    );
+  }
+}
+
+export default Meatstick;
