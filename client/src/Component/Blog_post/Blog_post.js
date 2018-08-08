b969a0ff30a424fb4fd4b2102ac481e340109fa1
John De Jesus <jdeje002@gmail.com>
2018-08-07 17:33:20 -0700

    added blog post

---
+++ client/src/Component/Blog_post/Blog_post.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import './Blog_post.css';
+import axios from 'axios'
+
+
+class Blog_post extends Component {
+  constructor(){
+      super();
+      this.state = { 
+        blogs:[]
+         }
+  }
+
+  componentDidMount() {
+    axios.get(`/blog_post`)
+      .then(res => {
+        
+        const blogs = res.data.map(obj => obj);
+        this.setState({ blogs })
+        
+      });
+  }
+    
+    
+    render() {
+        return(
+            <div className="blog_post"  >
+           
+               {this.state.blogs.map(blog =>(
+         <div class="row">
+         <div class="col s12 m6">
+           <div class="card blue-grey darken-1">
+             <div class="card-content white-text">
+               <span class="card-title">Reviews</span>
+               <p id ="card"key={blog._id}>{blog.subject}<br/>{blog.blog}</p>
+             </div>
+           </div>
+         </div>
+       </div>
+        
+                 ))} 
+            </div>
+            
+        )
+    }
+}
+
+export default Blog_post
