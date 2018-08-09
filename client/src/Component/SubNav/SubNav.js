c14d6ae4a722a13106312a84ee89fbc402773a56
Christina Wu <CHRISTINASARAHWU@GMAIL.COM>
2018-08-08 18:45:17 -0700

    added subnav links

--- client/src/Component/SubNav/SubNav.js
+++ client/src/Component/SubNav/SubNav.js
@@ -7,8 +7,8 @@ const SubNav = props =>
 <div className="nav-wrapper subNav">
   <ul id="nav-mobile" className="hide-on-med-and-down">
     <li><a className="subNavText mainSubNav" id={ props.id } href="#">{ props.title }</a></li>
-    <li><a className="subNavText" id="overview" href="#">Overview</a></li>
-    <li><a className="subNavText" id="specs" href="#">Specs</a></li>
+    <li><a className="subNavText" id="overview" href="#overviewTitle">Overview</a></li>
+    <li><a className="subNavText" id="specs" href="#specsTitle">Specs</a></li>
   </ul>
 </div>
 </nav>
